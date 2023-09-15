13771a3238ac2968bdb604a9ff99a1d2c09aab07
boytn080 <144509919+boytn080@users.noreply.github.com>
2023-09-15 11:18:25 +0530

    Add files via upload

--- main (2).py
+++ main (2).py
@@ -1,21 +1,39 @@
-# Python program to check if year is a leap year or not
+class BankAccount:
+    def __init__(self, account_number, account_holder_name, initial_balance=0.0):
+        self.__account_number = account_number
+        self.__account_holder_name = account_holder_name
+        self.__account_balance = initial_balance
 
-year = 2000
+    def deposit(self, amount):
+        if amount > 0:
+            self.__account_balance += amount
+            return f"Deposited ${amount}. New balance: ${self.__account_balance}"
+        else:
+            return "Invalid deposit amount. Amount must be greater than zero."
 
-# To get year (integer input) from the user
-# year = int(input("Enter a year: "))
+    def withdraw(self, amount):
+        if 0 < amount <= self.__account_balance:
+            self.__account_balance -= amount
+            return f"Withdrew ${amount}. New balance: ${self.__account_balance}"
+        elif amount > self.__account_balance:
+            return "Insufficient funds."
+        else:
+            return "Invalid withdrawal amount. Amount must be greater than zero."
 
-# divided by 100 means century year (ending with 00)
-# century year divided by 400 is leap year
-if (year % 400 == 0) and (year % 100 == 0):
-    print("{0} is a leap year".format(year))
+    def display_balance(self):
+        return f"Account balance for {self.__account_holder_name}: ${self.__account_balance}"
 
-# not divided by 100 means not a century year
-# year divided by 4 is a leap year
-elif (year % 4 ==0) and (year % 100 != 0):
-    print("{0} is a leap year".format(year))
 
-# if not divided by both 400 (century year) and 4 (not century year)
-# year is not leap year
-else:
-    print("{0} is not a leap year".format(year))
+# Example usage:
+if __name__ == "__main__":
+    # Create a BankAccount instance
+    account = BankAccount("123456789", "John Doe", 1000.0)
+
+    # Deposit money
+    print(account.deposit(500))  # Deposited $500. New balance: $1500.0
+
+    # Withdraw money
+    print(account.withdraw(200))  # Withdrew $200. New balance: $1300.0
+
+    # Display account balance
+    print(account.display_balance())  # Account balance for John Doe: $1300.0
